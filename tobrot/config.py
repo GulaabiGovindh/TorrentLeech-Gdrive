0a92a2e961bd73654e6db3f8ee6e15e4dc76e4fb
GulaabiGovindh <78079420+GulaabiGovindh@users.noreply.github.com>
2021-01-27 16:04:36 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,33 @@
+from tobrot.sample_config import Config
+
+#Fill your all data, read readme for reference
+
+#FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
+
+class Config(Config):
+
+    TG_BOT_TOKEN= "1339917934:AAF27G1hdH9VrRaqvdqfSjC6ZeA7saBVvnM"
+
+    APP_ID = 1045941
+
+    API_HASH = "91bb44f78565ac410d5c669e8cb40fb5"
+
+    OWNER_ID = 1034006204
+
+    AUTH_CHANNEL = [-1001423196556]
+
+    GLEECH_COMMAND = "gleech@torlech1kbot"
+    YTDL_COMMAND = "ytdl@torlech1kbot"
+    TELEGRAM_LEECH_COMMAND_G = "tleech@torlech1kbot"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
+    DESTINATION_FOLDER = "Gdrive bot"
+    LEECH_COMMAND = "leech@torhunter2_bot"
+    CANCEL_COMMAND_G = "cancel@torlech1kbot"
+    RCLONE_CONFIG = """"
+    
+
+
+
+
+
